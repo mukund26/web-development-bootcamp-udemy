9ae0f6e5baa2e0f7871c53c6ef9fc64c9c94a65d
JSewell-Git <jacksewellinfo@gmail.com>
2017-10-24 20:10:20 +0100

    Added index01.html & script01.js

---
+++ Learning JS/script01.js
@@ -0,0 +1,10 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+var userFullName = userFirstName + " " + userLastName;
+
+alert("Your full name is " + userFullName);
+console.log("Your full name is " + userFullName);
+
+alert("You're " + userAge + " years old");
+console.log("You're " + userAge + " years old");
