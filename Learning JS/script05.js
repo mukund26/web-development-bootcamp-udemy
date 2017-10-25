570f4146c206f0b322e40e98daf2d942aebdec0b
JSewell-Git <jacksewellinfo@gmail.com>
2017-10-25 16:00:17 +0100

    Added index05.html & script05.js

---
+++ Learning JS/script05.js
@@ -0,0 +1,40 @@
+console.log("Print all numbers between -10 & 19");
+
+var number = -10;
+
+while (number <= 19){
+	console.log(number);
+	number++;
+}
+
+console.log("Print all even numbers between 10 and 40")
+
+number = 10;
+
+while (number <= 40){
+	if (number % 2 === 0){
+		console.log(number);
+	}	
+	number++;
+}
+
+console.log("Print all odd numbers between 300 and 333");
+
+number = 300;
+ while(number <= 333){
+ 	if(number % 2 !== 0){
+ 		console.log(number);
+ 	}
+ 	number++
+ }
+
+ console.log("Print all numbers divisible by 5 AND 3 between 5 and 50");
+
+number = 5;
+
+ while(number <= 50){
+ 	if(number % 5 === 0 && number % 3 === 0){
+ 		console.log(number);
+ 	}
+ 	number++;
+ }
