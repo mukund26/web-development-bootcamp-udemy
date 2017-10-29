459e372ef33fda52bc422c127c39f33009328f0e
JSewell-Git <jacksewellinfo@gmail.com>
2017-10-29 19:13:49 +0000

    Added index07.html, index08.html, script07.js & script08.js

---
+++ Learning JS/script08.js
@@ -0,0 +1,15 @@
+function isEven(num){
+	return num % 2 === 0;
+}
+
+function factorial(num){
+	var result = 1;
+	for(var i = 2; i < num; i++){
+		result *= i;
+	}
+	return result;
+}
+
+function kebabToSnake(str) {
+	return str.replace(/-/g, "_");
+}
