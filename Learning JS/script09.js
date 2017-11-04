1bb157c167d44324c4435a15406d083e1c2018d7
JSewell-Git <jacksewellinfo@gmail.com>
2017-11-04 13:55:10 +0000

    Updated Todo app (index08.html & script09.js) with extra functionality

--- Learning JS/script09.js
+++ Learning JS/script09.js
@@ -6,12 +6,33 @@ var input = prompt("What would you like to do?");
 // Loop until user wants to quit application
 while (input !== "quit"){
 	if (input === "list") {
-		console.log(todos);
+		listTodos();
 	} else if (input === "new") {
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
+		addTodo();
+	} else if (input === "delete"){
+		deleteTodo();
 	}
 	input = prompt("What would you like to do?");
 }
 
 console.log("Okay, thank you for using the Todo app!");
+
+function listTodos(){
+	console.log("**********");
+	todos.forEach(function(todo, i){
+		console.log(i + ": " +todo);
+	});
+	console.log("**********");
+}
+
+function addTodo(){
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Todo added");
+}
+
+function deleteTodo(){
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index, 1);
+	console.log("Todo deleted");
+}
