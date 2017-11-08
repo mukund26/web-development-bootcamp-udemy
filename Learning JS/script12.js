3f50b73328f7779ba2ecd4f6867b1019a58be27e
JSewell-Git <29798942+JSewell-Git@users.noreply.github.com>
2017-11-08 15:58:43 +0000

    Added index12.html, script12.js & .gitignore

---
+++ Learning JS/script12.js
@@ -0,0 +1,6 @@
+var body = document.querySelector("body");
+var button = document.querySelector("#button");
+
+button.addEventListener("click", function(){
+	body.classList.toggle("purple-bg");
+});
